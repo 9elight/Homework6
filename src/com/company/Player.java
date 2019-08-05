c2f8fd8aff82038a26557ddbb24b723090f832cd
9elight <53308315+9elight@users.noreply.github.com>
2019-08-05 21:21:56 +0600

    Initial commit

---
+++ src/com/company/Player.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class Player {
+    private int health;
+    private int damage;
+    private int defence;
+    private int superDamage;
+
+
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getDefence() {
+        return defence;
+    }
+
+    public void setDefence(int defence) {
+        this.defence = defence;
+    }
+
+    public int getSuperDamage() {
+        return superDamage;
+    }
+
+    public void setSuperDamage(int superDamage) {
+        this.superDamage = superDamage;
+    }
+}
